5d8775a10798d5843f06a9169e26e169a88d928b
Guss <162443798+Guss2618@users.noreply.github.com>
2024-06-12 08:36:43 -0300

    Add files via upload

---
+++ boas-vindas.js
@@ -0,0 +1,28 @@
+var a = 1, b = 2, c = 3, z = a+b+c
+function boasvindas(){
+console.log("Boas Vindas")
+}
+
+function somar(){
+    console.log("A soma é ",(a + b))
+}
+
+function subtracao(){
+    console.log("A subtração é ",(a - b))
+}
+function multiplicacao(){
+    console.log("A multiplicação é ",(a * b))
+}
+function divisao(){
+    console.log("A divisão é ",(a / b))
+}
+function media(){
+    console.log("A media é ",(z/c))
+}
+
+boasvindas()
+somar()
+subtracao()
+multiplicacao()
+divisao()
+media()
